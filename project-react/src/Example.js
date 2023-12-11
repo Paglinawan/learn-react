1a99032035d1a39b9ecaa29bff2f2d21b55ffe57
miho <paglinawan0520@gmail.com>
2023-12-11 23:26:31 +0900

    Update: organize base files

---
+++ project-react/src/Example.js
@@ -0,0 +1,14 @@
+function Example() {
+  return (
+    <section className="sec">
+      <h2 className="sec-heading">Title</h2>
+      <p className="text-center">dammyTextdammyTextdammyTextdammyText</p>
+      <div className="sec-content">
+        <p className="sec-result">result</p>
+        <button className="sec-btn">click</button>
+      </div>
+    </section>
+  );
+}
+
+export default Example;
